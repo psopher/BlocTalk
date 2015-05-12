205105c6ed5b2dd816f1c1e38c0b5c12a0afe8f0
Philip Sopher <phsophe@gmail.com>
2015-05-11 20:03:51 -0400

    Initial Commit

---
+++ com.psopher.BlocTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.psopher.BlocTalk
+//
+//  Created by Philip Sopher on 5/11/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ com.psopher.BlocTalk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  com.psopher.BlocTalk
+//
+//  Created by Philip Sopher on 5/11/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
